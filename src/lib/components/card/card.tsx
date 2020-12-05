c18615f9ac28a09d654d44d108c6fe45f32d758a
ssengalanto <galantossen@gmail.com>
2020-12-05 10:53:05 +0800

    feat: create base label component

--- src/lib/components/card/card.tsx
+++ src/lib/components/card/card.tsx
@@ -5,13 +5,14 @@ export interface CardProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export const styles = {
   base: 'min-w-0 bg-white p-5 rounded-lg shadow-md',
+  default: 'bg-white dark:bg-gray-800',
 };
 
 export const Card = React.forwardRef<HTMLDivElement, CardProps>(function Card(
   { className, ...props },
   ref,
 ) {
-  const classes = clsx(styles.base, className);
+  const classes = clsx(styles.base, styles.default, className);
 
   return <div className={classes} ref={ref} {...props} />;
 });
---
+++ src/lib/components/label/label.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import clsx from 'clsx';
+
+export interface LabelProps extends React.HTMLAttributes<HTMLLabelElement> {
+  /**
+   * Displays the label inline
+   */
+  inline?: boolean;
+  /**
+   * Defines if the label is disabled
+   */
+  disabled?: boolean;
+}
+
+export const styles = {
+  base: 'block text-sm text-gray-700 dark:text-gray-400',
+  inline: 'inline-flex items-center',
+  disabled: 'opacity-50 cursor-not-allowed',
+};
+
+export const Label = React.forwardRef<HTMLLabelElement, LabelProps>(function Label(
+  { children, className, inline, ...props },
+  ref,
+) {
+  const classes = clsx(styles.base, inline && styles.inline, className);
+  return (
+    <label className={classes} ref={ref} {...props}>
+      {children}
+    </label>
+  );
+});
