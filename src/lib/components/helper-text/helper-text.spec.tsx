5739b75685517394c6dc24f6c062f76c759f2a3f
ssengalanto <galantossen@gmail.com>
2020-12-06 13:06:28 +0800

    test: create helper text component tests and stories

---
+++ src/lib/components/helper-text/helper-text.spec.tsx
@@ -0,0 +1,46 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import React from 'react';
+import faker from 'faker';
+import { screen, render } from '@testing-library/react';
+
+import { HelperText, HelperTextProps, styles } from './helper-text';
+
+const mockedProps: HelperTextProps = {};
+
+const testId = 'helper-component';
+
+const setup = (props: Partial<HelperTextProps> = {}) => {
+  const setupProps = { ...mockedProps, ...props };
+  return render(<HelperText data-testid={testId} {...setupProps} />);
+};
+
+describe('<HelperText />', () => {
+  it('should render without error', () => {
+    setup();
+    const helperText = screen.getByTestId(testId);
+
+    expect(helperText).toBeInTheDocument();
+  });
+
+  it('should render its children prop', () => {
+    const children = faker.random.word();
+    setup({ children });
+    const helperText = screen.getByText(children);
+
+    expect(helperText).toBeInTheDocument();
+  });
+
+  it('props { disabled: true }', () => {
+    setup({ disabled: true });
+    const helperText = screen.getByTestId(testId);
+
+    expect(helperText).toHaveClass(styles.disabled);
+  });
+
+  it('props { error: true }', () => {
+    setup({ error: true });
+    const helperText = screen.getByTestId(testId);
+
+    expect(helperText).toHaveClass(styles.error);
+  });
+});
---
+++ src/lib/components/helper-text/helper-text.stories.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable import/no-unresolved */
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import { HelperText, HelperTextProps } from './helper-text';
+
+export default {
+  title: 'Components/HelperText',
+  component: HelperText,
+  argTypes: {},
+} as Meta;
+
+const Template: Story<HelperTextProps> = (args) => <HelperText {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  children: 'Helper text',
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  children: 'Helper text',
+  disabled: true,
+};
+
+export const Error = Template.bind({});
+Error.args = {
+  children: 'Helper text',
+  error: true,
+};
