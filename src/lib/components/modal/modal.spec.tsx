8e4ff9974e84353717d736b7c5bafa9790e92b20
ssengalanto <galantossen@gmail.com>
2020-12-08 22:54:34 +0800

    test: create modal component tests and stories

---
+++ src/lib/components/modal/modal.spec.tsx
@@ -0,0 +1,51 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { MockComponent, mockComponentTestId } from 'lib/test-utils';
+
+import { Modal, ModalProps } from './modal';
+
+const mockedProps: ModalProps = {
+  open: false,
+  onClose: jest.fn(),
+};
+
+const setup = (props: Partial<ModalProps> = {}) => {
+  const setupProps = { ...mockedProps, ...props };
+  return render(
+    <Modal {...setupProps}>
+      <MockComponent />
+    </Modal>,
+  );
+};
+
+describe('<Modal />', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('props { open: true }', () => {
+    setup({ open: true });
+    const modal = screen.getByRole('dialog');
+    const mockComponent = screen.getByTestId(mockComponentTestId);
+
+    expect(modal).toBeInTheDocument();
+    expect(modal).toContainElement(mockComponent);
+  });
+
+  it('props { open: false }', () => {
+    const modal = screen.queryByRole('dialog');
+    expect(modal).not.toBeInTheDocument();
+  });
+
+  it('it should call onClose prop when the close button is clicked', () => {
+    setup({ open: true });
+    const button = screen.getByRole('button', { name: /close/i });
+
+    userEvent.click(button);
+
+    expect(mockedProps.onClose).toHaveBeenCalledTimes(1);
+  });
+});
---
+++ src/lib/components/modal/modal.stories.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable import/no-unresolved */
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import { Modal, ModalProps } from './modal';
+
+export default {
+  title: 'Components/Modal',
+  component: Modal,
+  argTypes: {},
+} as Meta;
+
+const Template: Story<ModalProps> = (args) => <Modal {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  children: 'your modal content',
+  open: true,
+};
